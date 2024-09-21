jingfelix/Repo2Lark

fix: reduce number of workers in Dockerfile
Signed-off-by: jingfelix <jingfelix@outlook.com>

Dockerfile
@@ -14,4 +14,4 @@ ENV PYTHONUNBUFFERED=1
 COPY requirements.txt .
 RUN python -m pip install --no-cache-dir --upgrade repo2lark
 
-CMD ["uvicorn", "repo2lark:app", "--port", "3030", "--host", "0.0.0.0", "--workers", "4"]
+CMD ["uvicorn", "repo2lark:app", "--port", "3030", "--host", "0.0.0.0", "--workers", "2"]

hust-open-atom-club/Repo2Kook

Dockerfile
@@ -14,4 +14,4 @@ ENV PYTHONUNBUFFERED=1
 COPY requirements.txt .
 RUN python -m pip install --no-cache-dir --upgrade repo2kook
 
-CMD ["uvicorn", "repo2kook:app", "--port", "3030", "--host", "0.0.0.0", "--workers", "4"]
+CMD ["uvicorn", "repo2kook:app", "--port", "3030", "--host", "0.0.0.0", "--workers", "2"]